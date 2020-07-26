5a7fbb5e3d8e91f7486f6797045c9e025a19041a
Sam Odle <sro27@cornell.edu>
2020-07-26 14:35:36 -0400

    finding n nearest neigbors for each distance type for each profession

---
+++ Analytics/ProfessionNearestNeighbors.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Analytics
+{
+    public class ProfessionNearestNeighbors
+    {
+        public string Name { get; set; }
+        public List<Tuple<Constants.AttributeType, List<string>>> NearestNeighbors { get; set; } = new List<Tuple<Constants.AttributeType, List<string>>>();
+
+        public ProfessionNearestNeighbors(string name)
+        {
+            this.Name = name;
+        }
+
+
+    }
+}
--- Raw_Job_Processing/Program.cs
+++ Raw_Job_Processing/Program.cs
@@ -6,6 +6,7 @@ using DataPersistancy;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
 using MongoDB.Driver;
+using static Analytics.Constants;
 using Attribute = Analytics.Attribute;
 
 namespace Raw_Job_Processing
@@ -20,7 +21,7 @@ namespace Raw_Job_Processing
 
             //removeDupes();
 
-            ONET_ETL_Profession();
+            PopulateAllProfessionNearestNeighbors(20);
 
 
 
@@ -28,6 +29,72 @@ namespace Raw_Job_Processing
         }
 
 
+        //
+        private static void PopulateAllProfessionNearestNeighbors(int n)
+        {
+            MongoClient dbClient = new MongoClient("mongodb://forkAdmin:ForkAdmin123@localhost:27017");
+            IMongoDatabase database = dbClient.GetDatabase("graphs");
+
+            var edge_collection = database.GetCollection<BsonDocument>("edges_professions");
+            var destination_collection = database.GetCollection<BsonDocument>("nearest_neighbors_profession");
+
+            var ForkReport = new ONETReport();
+            ForkReport.MasterOccupationList = JSON_IO.Import_OccupationList(Helper.Publics.FILENAMES.OCCUPATIONS + ".txt");
+
+
+            foreach (Occupation o in ForkReport.MasterOccupationList)
+            {
+
+                var occupationFilterA = Builders<BsonDocument>.Filter.Eq("OccupationAName", o.Name);
+                var targetAdjacenciesA = edge_collection.Find(occupationFilterA).ToList();
+
+                var occupationFilterB = Builders<BsonDocument>.Filter.Eq("OccupationBName", o.Name);
+                var targetAdjacenciesB = edge_collection.Find(occupationFilterB).ToList();
+
+                var MasterAdjacencyList = new List<SimpleOccupationEdge>();
+
+                //retrieve adjacencies from database
+                foreach (var a in targetAdjacenciesA)
+                {
+                    MasterAdjacencyList.Add(BsonSerializer.Deserialize<SimpleOccupationEdge>(a));
+                }
+                foreach (var b in targetAdjacenciesB)
+                {
+                    MasterAdjacencyList.Add(BsonSerializer.Deserialize<SimpleOccupationEdge>(b));
+                }
+
+
+                ProfessionNearestNeighbors newNeighborList = new ProfessionNearestNeighbors(o.Name);
+
+                //sort adjacencies
+                foreach (AttributeType foo in Enum.GetValues(typeof(AttributeType)))
+                {
+                    if (foo == AttributeType.Word2VecWIKI || foo == AttributeType.Skill || foo == AttributeType.Net || foo == AttributeType.Knowledge || foo == AttributeType.Ability) {
+                        List<SimpleOccupationEdge> SortedAdjacencyList = MasterAdjacencyList.OrderBy(oy => oy.getDistance(foo)).ToList();
+                        var newList = SortedAdjacencyList.Take(n).Select(c => c.getOtherName(o.Name)).ToList();
+
+                        newNeighborList.NearestNeighbors.Add(new Tuple<AttributeType, List<string>>(foo, newList));
+                    }
+                }
+
+                destination_collection.InsertOne(newNeighborList.ToBsonDocument());
+
+            }
+
+
+
+            //save to DB
+
+
+            var EmpInfoArray = new List<BsonDocument>();
+            foreach (Occupation j in ForkReport.MasterOccupationList)
+            {
+                EmpInfoArray.Add(j.ToBsonDocument());
+            }
+            destination_collection.InsertMany(EmpInfoArray);
+        }
+
+        //ETL the profession info, add top adjacencies to profession node (this part is bad and needs to go)
         private static void ONET_ETL_Profession()
         {
             var ForkReport = new ONETReport();
@@ -76,7 +143,8 @@ namespace Raw_Job_Processing
 
 
                 //save top adjacencies
-                o.TenMostAdjacent = SortedAdjacencyList.Take(10).Select(c => c.getOtherName(o.Name)).ToList();
+                //REMOVING - NOT HOW WE ARE MANAGING THIS INFO 
+                //o.TenMostAdjacent = SortedAdjacencyList.Take(10).Select(c => c.getOtherName(o.Name)).ToList();
             }
 
 
@@ -93,6 +161,8 @@ namespace Raw_Job_Processing
         }
 
 
+
+        //ETL the ONET data from origial ONET dowloadable db format to MongoDB
         private static void ONET_ETL_NoProfession()
         {
             var ForkReport = new ONETReport();
